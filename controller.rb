b0dde6e432c594fa4865dc79561621c83c4c3d66
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-04-22 12:28:21 -0500

    ablock demo

--- controller.rb
+++ controller.rb
@@ -2,7 +2,33 @@ require 'sinatra'
 
 
 get '/' do
+	@title = "Let's Play Monopoly!"
+	@story = "Choose your token"
+	@choice_a = "hat"
+	@choice_b = "iron"
+	erb :index
+end
 
+get '/direction/:choice' do
+	case params[:choice]
+	when "hat"
+			@title = "Landed on Kings Cross Station"
+			@story = "Would you like to buy or save your money?"
+			@choice_a = "buy"
+			@choice_b = "save"
+			erb :index
+	when "buy"
+			@title = "Landed on Kings Cross Station"
+			@story = "Would you like to buy or save your money?"
+			@choice_a = "buy"
+			@choice_b = "save"
+			erb :index
+	when "iron"
+		@title = "You Lose"
+		@ending = "Iron never wins"
+		erb :ending
+	end
+		
 end
 
 
